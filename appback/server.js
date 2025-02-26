f671e9c511feb43d94d4ba79155476aa534b3ca1
kaaris <kaaris@gmail.com>
2025-02-26 14:59:58 +0100

    serv back commit

--- appback/server.js
+++ appback/server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+require('dotenv').config();  
+
 
 const app = express();
-const PORT = 20000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connexion à la base de données
 const db = mysql.createConnection({
-    host: '192.168.65.249',
-    user: 'root',
-    password: 'root',
-    database: 'tournois_futsal'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données:', err.stack);
+        return;
+    }
+    console.log('Connecté à la base de données avec succès!');
+});
+
 
 // Vérifier la connexion
 db.connect(err => {
